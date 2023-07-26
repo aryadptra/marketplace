2dd1177c0395b801135ed0dc5f75d72c8961f643
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-07-26 13:07:21 +0700

    order admin on progress

---
+++ resources/views/pages/admin/order/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('title')
+    Store Dashboard
+@endsection
+
+@section('content')
+    <div class="section-content section-dashboard-home" data-aos="fade-up">
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">Order</h2>
+                <p class="dashboard-subtitle">
+                    List of Order Transactions
+                </p>
+            </div>
+            <div class="dashboard-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
